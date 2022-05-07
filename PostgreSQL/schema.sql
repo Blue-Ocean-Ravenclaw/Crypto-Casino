7f13a739c70da3b617112c747b3d1c9390be4313
varunsrinivasan2 <varunsrinivasan2@gmail.com>
2022-05-07 12:17:49 -0700

    fake data

--- PostgreSQL/schema.sql
+++ PostgreSQL/schema.sql
@@ -1,5 +1,6 @@
 --create db
-CREATE DATABASE blueocean
+DROP DATABASE blueocean;
+CREATE DATABASE blueocean;
 
 --connect to the db
 \c blueocean
@@ -12,16 +13,19 @@ CREATE DATABASE blueocean
 --
 -- ---
 
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS users;
 
-CREATE TABLE user (
-  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY UNIQUE NOT NULL,
   username VARCHAR NULL DEFAULT NULL,
   tokens BIGINT NULL DEFAULT NULL,
   phone_number VARCHAR NULL DEFAULT NULL,
-  wallet_address VARCHAR NULL DEFAULT NULL,
-  PRIMARY KEY (id)
+  wallet_address VARCHAR NULL DEFAULT NULL
 );
+COPY users(id, username, tokens, phone_number, wallet_address)
+FROM '/Users/varun/Documents/rfp2202/BlueOcean/Blue-Ocean-Ravenclaw/fakeData/user.csv'
+DELIMITER ','
+CSV HEADER;
 
 -- ---
 -- Table 'nfts'
@@ -31,15 +35,18 @@ CREATE TABLE user (
 DROP TABLE IF EXISTS nfts;
 
 CREATE TABLE nfts (
-  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  id SERIAL PRIMARY KEY UNIQUE NOT NULL,
   id_user INTEGER NULL DEFAULT NULL,
   description VARCHAR NULL DEFAULT NULL,
   external_url VARCHAR NULL DEFAULT NULL,
   image VARCHAR NULL DEFAULT NULL,
   name VARCHAR NULL DEFAULT NULL,
-  value INT NULL DEFAULT NULL,
-  PRIMARY KEY (id)
+  value INT NULL DEFAULT NULL
 );
+COPY nfts(id, id_user, description, external_url, image, name, value)
+FROM '/Users/varun/Documents/rfp2202/BlueOcean/Blue-Ocean-Ravenclaw/fakeData/nfts.csv'
+DELIMITER ','
+CSV HEADER;
 
 -- ---
 -- Table 'card_inventory'
@@ -49,19 +56,22 @@ CREATE TABLE nfts (
 DROP TABLE IF EXISTS card_inventory;
 
 CREATE TABLE card_inventory (
-  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  id SERIAL PRIMARY KEY UNIQUE NOT NULL,
   id_user INTEGER NULL DEFAULT NULL,
   card_name VARCHAR NULL DEFAULT NULL,
-  quantity INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (id)
+  quantity INTEGER NULL DEFAULT NULL
 );
+COPY card_inventory(id, id_user, card_name, quantity)
+FROM '/Users/varun/Documents/rfp2202/BlueOcean/Blue-Ocean-Ravenclaw/fakeData/card_inventory.csv'
+DELIMITER ','
+CSV HEADER;
 
 -- ---
 -- Foreign Keys
 -- ---
 
-ALTER TABLE nfts ADD FOREIGN KEY (id_user) REFERENCES user (id);
-ALTER TABLE card_inventory ADD FOREIGN KEY (id_user) REFERENCES user (id);
+ALTER TABLE nfts ADD FOREIGN KEY (id_user) REFERENCES users (id);
+ALTER TABLE card_inventory ADD FOREIGN KEY (id_user) REFERENCES users (id);
 
 -- ---
 -- Table Properties
