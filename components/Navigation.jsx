b96307d75dffcbefc0a616c00499a7e1dd627a5b
Bradford Trevino <bradford.w.trevino@gmail.com>
2022-05-08 08:23:55 -0700

    Completed navigation!

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -3,7 +3,7 @@ import React, {
 } from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Link from '@mui/material/Link';
+import { useRouter } from 'next/router';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import PersonIcon from '@mui/icons-material/Person';
@@ -12,7 +12,13 @@ import GamesIcon from '@mui/icons-material/Games';
 import CasinoIcon from '@mui/icons-material/Casino';
 
 function Navigation() {
-  const [value, setValue] = React.useState(0);
+  const router = useRouter();
+
+  const [value, setValue] = React.useState(router.pathname.slice(1));
+
+  const onLink = (href) => {
+    router.push(href);
+  };
 
   return (
     <Paper sx={{ position: 'fixed', bottom: 10, left: 10, right: 10 }} elevation={3}>
@@ -22,10 +28,33 @@ function Navigation() {
           setValue(newValue);
         }}
       >
-        <BottomNavigationAction label="User" value="user" icon={<PersonIcon color={value === 'user' ? 'primary' : 'secondary'}/>} />
-        <BottomNavigationAction label="Wallet" value="wallet" icon={<AccountBalanceWalletIcon color={value === 'wallet' ? 'primary' : 'secondary'}/>} />
-        <BottomNavigationAction label="Games" value="games" icon={<GamesIcon color={value === 'games' ? 'primary' : 'secondary'}/>} />
-        <BottomNavigationAction label="Play" value="play" icon={<CasinoIcon color={value === 'play' ? 'primary' : 'secondary'}/>} />
+        <BottomNavigationAction
+          label="User"
+          value="user"
+          icon={<PersonIcon color={value === 'user' ? 'primary' : 'secondary'}/>}
+          onClick={() => onLink('user')}
+        />
+
+        <BottomNavigationAction
+          label="Wallet"
+          value="wallet"
+          icon={<AccountBalanceWalletIcon color={value === 'wallet' ? 'primary' : 'secondary'}/>}
+          onClick={() => onLink('wallet')}
+        />
+
+        <BottomNavigationAction
+          label="Games"
+          value="games"
+          icon={<GamesIcon color={value === 'games' ? 'primary' : 'secondary'}/>}
+          onClick={() => onLink('games')}
+        />
+
+        <BottomNavigationAction
+          label="Play"
+          value="play"
+          icon={<CasinoIcon color={value === 'play' ? 'primary' : 'secondary'}/>}
+          onClick={() => onLink('play')}
+        />
       </BottomNavigation>
     </Paper>
   );
