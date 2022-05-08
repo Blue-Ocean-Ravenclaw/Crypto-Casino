62d0af428006be31adff108bbe0e4c2fccb03828
Calvin See <calvinfrancissee@gmail.com>
2022-05-07 18:50:37 -0700

    Reworked dice reveal

--- components/games/dice/Dice.jsx
+++ components/games/dice/Dice.jsx
@@ -1,10 +1,24 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
-export default function ({roll, revealState, reveal}) {
+export default function ({roll, setTotalRevealed, totalRevealed, diceArr}) {
+  const [revealed, setRevealed] = useState(false);
+
+  useEffect(() => { //Reset to hidden on new roll
+    setRevealed((prev) => prev ? false : prev);
+  }, [diceArr]);
+  useEffect(() => { //When revealed update total revealed
+    if (revealed) {
+      setTotalRevealed();
+    }
+  }, [revealed]);
+
+  function reveal () {
+      setRevealed((prev) => !prev ? true : prev);
+  }
+
   return (
     <div className="dice" onClick={reveal}>
-      <h3>Dice: <button>{revealState ? roll : 'Show'}</button></h3>
-
+      <h3>Dice: <button>{revealed && totalRevealed !== 0 ? roll : 'Show'}</button></h3>
     </div>
   )
 }
--- components/games/dice/DiceGame.jsx
+++ components/games/dice/DiceGame.jsx
@@ -5,39 +5,12 @@ import axios from 'axios';
 export default function DiceGame ({plays, luck, playGame, playing}) {
   const initialState = { //Initial Game State
     diceArr: [],
-    revealed: 0,
-    revealState: {
-      one: false,
-      two: false,
-      three: false
-    }
+    revealed: 0
   }
   function reducer (state, action) { //Controls the Game State
     switch (action.type) {
-      case 'revealOne':
-        if (!state.revealState.one) {
-          let newRevealState = {...state.revealState, one: true};
-          let newRevealed = state.revealed + 1;
-          return {...state, revealed: newRevealed, revealState: newRevealState};
-        } else {
-          return state;
-        }
-      case 'revealTwo':
-        if (!state.revealState.two) {
-          let newRevealState = {...state.revealState, two: true};
-          let newRevealed = state.revealed + 1;
-          return {...state, revealed: newRevealed, revealState: newRevealState};
-        } else {
-          return state;
-        }
-      case 'revealThree':
-        if (!state.revealState.three) {
-          let newRevealState = {...state.revealState, three: true};
-          let newRevealed = state.revealed + 1;
-          return {...state, revealed: newRevealed, revealState: newRevealState};
-        } else {
-          return state;
-        }
+      case 'reveal':
+        return {...state, revealed: state.revealed + 1};
       case 'roll':
         let newDice = rollDice(plays, luck);
         let hidden = {one: false, two: false, three: false};
@@ -56,17 +29,14 @@ export default function DiceGame ({plays, luck, playGame, playing}) {
 
   //Wrapping the reveal functions to pass to the Dice Components
   //Prevent unnecessary re-rendering of Dice
-  const revealOne = useCallback(() => {dispatch({type: 'revealOne'})}, []);
-  const revealTwo = useCallback(() => {dispatch({type: 'revealTwo'})}, []);
-  const revealThree = useCallback(() => {dispatch({type: 'revealThree'})}, []);
+  const setTotalRevealed = useCallback(() => {dispatch({type: 'reveal'})}, []);
 
   function renderDice () {
     if (diceState.diceArr.length === 3) {
       return (
         <div className="dice-container">
-          <Dice roll={diceState.diceArr[0]} revealState={diceState.revealState.one} reveal={revealOne} />
-          <Dice roll={diceState.diceArr[1]} revealState={diceState.revealState.two} reveal={revealTwo} />
-          <Dice roll={diceState.diceArr[2]} revealState={diceState.revealState.three} reveal={revealThree} />
+          {diceState.diceArr.map((roll) =>
+          <Dice roll={roll} setTotalRevealed={setTotalRevealed} totalRevealed={diceState.revealed} diceArr={diceState.diceArr} /> )}
         </div>
         )
       }
@@ -74,6 +44,7 @@ export default function DiceGame ({plays, luck, playGame, playing}) {
 
   return (
     <div>
+      <h3>Revealed: {diceState.revealed}</h3>
       {plays > 0 ? <button onClick={playGame}>Roll The Dice</button> : 'Buy More!'}
       {renderDice()}
     </div>
