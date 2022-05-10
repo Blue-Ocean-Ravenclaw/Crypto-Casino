52f47a89187ad6a3c3fe03eab2b3cd45a757040f
ivyhu <ajianouc@gmail.com>
2022-05-10 16:14:42 -0700

    added auto-scroll to bottom

--- pages/chat/index.jsx
+++ pages/chat/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import io from "Socket.IO-client";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
@@ -13,8 +13,19 @@ const Home = () => {
   const [userSubmitted, setUserSubmitted] = useState(false);
   const [msgList, setMsgList] = useState([]);
   const [userId, setUserId] = useState("");
+  const messagesEndRef = useRef(null);
 
-  useEffect(() => socketInitializer(), []);
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    return socketInitializer();
+  }, []);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [msgList]);
 
   const socketInitializer = async () => {
     await fetch("/api/socket");
@@ -63,66 +74,141 @@ const Home = () => {
     return color;
   };
 
+  const generateMsgDisplay = () => {
+    const msgDisplay = [];
+    msgList.forEach((msgObj, index) => {
+      // handle when msg is from client
+      if (msgObj.userId === userId) {
+        msgDisplay.push(
+          <div key={index}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+                flexDirection: "row",
+                pr: 3,
+              }}
+            >
+              <Box
+                sx={{
+                  pl: 3,
+                }}
+              >
+                {msgObj.msg}
+              </Box>
+              <Typography
+                align="left"
+                color={usernameColor[msgObj.username]}
+                style={{
+                  fontWeight: 600,
+                  fontSize: "1rem",
+                  margin: " 0rem 0rem 0rem 0.5rem",
+                }}
+              >
+                {msgObj.username}
+              </Typography>
+            </Box>
+          </div>
+        );
+      } else {
+        // handle when msg is from others
+        msgDisplay.push(
+          <div key={index}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-start",
+                flexDirection: "row",
+                pl: 3,
+              }}
+            >
+              <Typography
+                align="left"
+                color={usernameColor[msgObj.username]}
+                style={{
+                  fontWeight: 600,
+                  fontSize: "1rem",
+                  margin: " 0rem 0.5rem 0rem 0rem",
+                }}
+              >
+                {msgObj.username}
+              </Typography>
+              <Box
+                sx={{
+                  pr: 3,
+                }}
+              >
+                {msgObj.msg}
+              </Box>
+            </Box>
+          </div>
+        );
+      }
+    });
+    // add a dummy node for autoscroll
+    msgDisplay.push(<div key="dummy" ref={messagesEndRef} />);
+    return msgDisplay;
+  };
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        // alignItems: "center",
-        flexDirection: "column",
-        padding: 2,
-        fontSize: "0.8rem",
-      }}
-    >
-      <h1>Chat History</h1>
-      {msgList.map((msgObj) => (
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "flex-start",
-            // justifyContent: "space-between",
-            flexDirection: "row",
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          padding: 2,
+          fontSize: "1rem",
+        }}
+      >
+        <h1>Chat Room</h1>
+        <div
+          style={{
+            width: "375px",
+            height: "420px",
+            overflow: "scroll",
           }}
         >
-          <Typography
-            align="left"
-            color={usernameColor[msgObj.username]}
-            style={{
-              fontWeight: 600,
-              fontSize: "0.8rem",
-              margin: " 0rem 0.5rem 0rem 0rem",
-            }}
-          >
-            {msgObj.username}
-          </Typography>
-          <Box>{msgObj.msg}</Box>
-        </Box>
-      ))}
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Start chatting"
-          value={msg}
-          onChange={onChangeHandler}
-        />
-        <Button variant="contained" onClick={handleSubmit}>
-          Submit
-        </Button>
-      </form>
-      {userSubmitted ? (
-        <div>Hello {username}</div>
-      ) : (
-        <form onSubmit={handleUsername}>
-          <input
-            placeholder="Tell us your display"
-            value={username}
-            onChange={onChangeHandlerUserName}
-          />
-          <Button variant="contained" onClick={handleUsername}>
-            Submit
-          </Button>
-        </form>
-      )}
-    </Box>
+          {generateMsgDisplay()}
+        </div>
+        {!userSubmitted ? (
+          ""
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <input
+              style={{
+                width: "320px",
+              }}
+              placeholder="Start chatting"
+              value={msg}
+              onChange={onChangeHandler}
+            />
+          </form>
+        )}
+        {userSubmitted ? (
+          ""
+        ) : (
+          <form onSubmit={handleUsername}>
+            <input
+              placeholder="Enter your name"
+              value={username}
+              onChange={onChangeHandlerUserName}
+            />
+            <Button
+              variant="contained"
+              sx={{
+                height: 25,
+                margin: 1,
+              }}
+              onClick={handleUsername}
+            >
+              Submit
+            </Button>
+          </form>
+        )}
+      </Box>
+    </>
   );
 };
 
