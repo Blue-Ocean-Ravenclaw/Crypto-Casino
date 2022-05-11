3a4dbd3074e5e54ab0beeda36be3db27d0d4ba0a
Timothy <timnguy17@gmail.com>
2022-05-11 11:51:23 -0700

    changed nft carousel to games

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "../../context/AuthContext.js";
+import { useAppContext } from '../../context/state.js';
 
 import Carousel from 'react-material-ui-carousel'
 import { Paper, Grid } from '@mui/material'
@@ -8,6 +9,12 @@ import { Paper, Grid } from '@mui/material'
 export default function User() {
   const router = useRouter();
   const { currentUser } = useAuth();
+  const { username } = useAppContext();
+  // const { } = useAppContext();
+
+  const onLink = (href) => {
+    router.push(href);
+  };
 
   // Only allows logged in user to access this page
   if (!currentUser) {
@@ -16,34 +23,22 @@ export default function User() {
 
   let sampleNfts = [
     {
-      "image": "https://i.gyazo.com/d86e26ca5e770ffaadf4a718e6712870.png",
-      "value": 1,
-      "name": "Lord 1",
-      "description": "My NFT 1"
+      "image": "https://i.gyazo.com/8d6cb9ecfc6086cc012e668a1ea8ca22.png",
+      "name": "Lucky Lucy"
     },
     {
-      "image": "https://i.gyazo.com/f6dbcbc81ea85bc7c5ec5c0ee3e3525e.png",
-      "value": 1,
-      "name": "Swag NFT 1",
-      "description": "Swag NFT 1"
+      "image": "https://i.gyazo.com/1690a9467a2da075402693412f4cb162.png",
+      "name": "Bingo"
     },
     {
-      "image": "https://i.gyazo.com/542eb14c971d634c37233359c388cddf.jpg",
-      "value": 1,
-      "name": "Crazy NFT 1",
-      "description": "Crazy NFT 1"
-    },
-    {
-      "image": "https://i.gyazo.com/bdbddb50508b7aadb51b895a822b254f.jpg",
-      "value": 1,
-      "name": "Crunk NFT 1",
-      "description": "Crunk NFT 1"
+      "image": "https://i.gyazo.com/1563373bca281a21cf4bdeea575bf23e.png",
+      "name": "High Roller"
     }
   ];
 
   return (
     <div>
-      User Page
+      <span>{username} Tokens: {currentUser.token}</span>
       <Grid container direction='column' justifyContent="space-around" alignItems='center'>
         <Carousel sx={{ width: .8 }} navButtonsAlwaysVisible={true}>
           {
@@ -71,7 +66,7 @@ function Item(props) {
   return (
     <Paper height='200'>
       <Grid container justifyContent='center'>
-        < img src={props.item.image} width='180' height='180' ></img>
+        < img src={props.item.image} width='auto' height='175' onClick={() => onLink('/play')}></img>
       </Grid >
     </Paper>
   )
