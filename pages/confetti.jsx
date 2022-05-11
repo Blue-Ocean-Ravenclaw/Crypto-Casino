14cc001fc9769471d9b1447363e87e11c69ea89d
ivyhu <ajianouc@gmail.com>
2022-05-11 11:40:33 -0700

    add confetti component

--- pages/confetti.jsx
+++ pages/confetti.jsx
@@ -1,6 +1,6 @@
 import { realConfetti, fireWorksConfetti } from "../components/confetti.jsx";
 export default function Confetti() {
-  realConfetti(false);
-  // fireWorksConfetti(true);
+  // realConfetti(false);
+  fireWorksConfetti(true);
   return <div>confetti</div>;
 }
