e8d51023a1a3a02873acc479c60e0c008038d318
Calvin See <calvinfrancissee@gmail.com>
2022-05-11 20:37:33 -0700

    Converted LadyLuck to a Reducer

--- components/games/ladyluck/LadyLuck.jsx
+++ components/games/ladyluck/LadyLuck.jsx
@@ -1,20 +1,11 @@
 import { getLadyLuck } from "../../../lib/ladyLuck.js";
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, useReducer } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import LLBoard from "./LLBoard.jsx";
 import LLPlayerNum from "./LLPlayerNum.jsx";
 import Modal from '@mui/material/Modal';
 import { useRouter } from "next/router";
-
-const processBoard = (array) => {
-  return [
-    array.slice(0, 5),
-    array.slice(5, 10),
-    array.slice(10, 15),
-    array.slice(15, 20),
-  ];
-};
 
 export default function LadyLuck({ plays, playGame, playing }) {
   const initialState = {
@@ -57,92 +48,21 @@ export default function LadyLuck({ plays, playGame, playing }) {
   const revealed = useCallback(() => dispatch({ type: "revealed" }), []);
   const toggleModal = () => dispatch({type: 'toggleModal'});
 
-  const [board, setBoard] = useState([]);
-  const [playerNums, setPlayerNums] = useState([]);
-  const [outcomes, setOutcomes] = useState({});
-  const [revealed, setRevealed] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const [prize, setPrize] = useState('');
-  const reveal = useCallback(
-    () =>
-      setCounter((prev) => {
-        if (prev < 24) {
-          return prev + 1;
-        } else {
-          setRevealed(true);
-          return prev;
-        }
-      }),
-    []
-  );
-
-  useEffect(() => {
-    if (playing) {
-      const game = getLadyLuck();
-      setBoard(processBoard(game.board));
-      setPlayerNums(game.playerNums);
-      setOutcomes(game.winDistribution);
-      setPrize(game.prize);
-      setRevealed(false);
-      setCounter(0);
+  const reveal = useCallback(() => {
+    if (game.counter < 24) {
+      dispatch({type: 'count'});
+    } else {
+      dispatch({type: 'revealed'});
     }
-  }, [plays]);
+  }, []);
 
-  function playLadyLuck() {
-    axios
-      .get(`/play/ladyLuck/roll?user_id=${1}`)
-      .then((res) => {
-        setBoard(processBoard(res.data.game.board));
-        setPlayerNums(res.data.game.playerNums);
-        setOutcomes(res.data.game.winDistribution);
-        setRevealed(false);
-        setCounter(0);
-      })
-      .catch((res) => {
-        setBoard([]);
-        setPlayerNums([]);
-        setOutcomes({});
-        setRevealed(false);
-        setCounter(0);
-      });
+  function play () {
+    const newGame = getLadyLuck();
+    dispatch({type: 'play', payload: newGame});
   }
 
   const displayPrize = () => {
-    const prizeStyle = {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderRadius: '2px'
-
-    };
-    const prizeMessages = {
-      'grandPrize': {
-        header: 'GRAND PRIZE!!',
-        message: "JACKPOT!!! You won an NFT!"
-      },
-      'doubleSeconds': {
-        header: "SECOND PRIZE - AND MORE!",
-        message: "Hoooo-eee, we've got a winner! You've won 20x your tokens back!"
-      },
-      'doubleThirds': {
-        header: 'DOUBLE THE LUCK, DOUBLE THE FUN!',
-        message: "No kidding - you scored a double win! You've won 10x your tokens back!"
-      },
-      'second' : {
-        header: 'YOU WON!',
-        message: "Luck is in the air! You've won 5x your tokens back!"
-      },
-      'third' : {
-        header: 'YOU WON!',
-        message: "Nicely done! You've won your tokens back!"
-      },
-      'loser': {
-        header: 'So close!',
-        message: 'Not this time! Play again!'
-      }
-    };
-    const { header, message }= prizeMessages[prize];
+    const { header, message }= prizeMessages[game.prize];
     return (
       <Box sx = {prizeStyle}>
         <h1>{header}</h1>
@@ -150,10 +70,6 @@ export default function LadyLuck({ plays, playGame, playing }) {
       </Box>
     );
   }
-
-  const toggleModal = () => {
-    setRevealed(!revealed);
-  };
 
   return (
     <Box
@@ -171,7 +87,7 @@ export default function LadyLuck({ plays, playGame, playing }) {
           bgcolor: "ladyLuck.main",
         }}
         variant="contained"
-        onClick={playGame}
+        onClick={play}
       >
         New Game
       </Button>
@@ -184,18 +100,18 @@ export default function LadyLuck({ plays, playGame, playing }) {
           marginBottom: 5,
         }}
       >
-        {playerNums.map((num, i) => (
+        {game.playerNums.map((num, i) => (
           <LLPlayerNum
             key={i}
-            playerNums={playerNums}
+            playerNums={game.playerNums}
             num={num}
             reveal={reveal}
           />
         ))}
       </Box>
-      <LLBoard board={board} reveal={reveal} />
+      <LLBoard board={game.board} reveal={reveal} />
       <Modal
-          open = {revealed}
+          open = {game.revealed}
           onClose ={toggleModal}
           sx = {{
             display: 'flex',
@@ -211,9 +127,44 @@ export default function LadyLuck({ plays, playGame, playing }) {
             width: 400,
             height: 500
           }}>
-            { prize.length ? displayPrize() : null}
+            { game.prize.length ? displayPrize() : null}
           </Box>
         </Modal>
     </Box>
   );
 }
+
+const prizeStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: '2px'
+
+};
+const prizeMessages = {
+  'grandPrize': {
+    header: 'GRAND PRIZE!!',
+    message: "JACKPOT!!! You won an NFT!"
+  },
+  'doubleSeconds': {
+    header: "SECOND PRIZE - AND MORE!",
+    message: "Hoooo-eee, we've got a winner! You've won 20x your tokens back!"
+  },
+  'doubleThirds': {
+    header: 'DOUBLE THE LUCK, DOUBLE THE FUN!',
+    message: "No kidding - you scored a double win! You've won 10x your tokens back!"
+  },
+  'second' : {
+    header: 'YOU WON!',
+    message: "Luck is in the air! You've won 5x your tokens back!"
+  },
+  'third' : {
+    header: 'YOU WON!',
+    message: "Nicely done! You've won your tokens back!"
+  },
+  'loser': {
+    header: 'So close!',
+    message: 'Not this time! Play again!'
+  }
+};
