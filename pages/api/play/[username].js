3668964f3194e7b3c252116aceec9cec258cfe6d
Calvin See <calvinfrancissee@gmail.com>
2022-05-11 18:54:27 -0700

    Subtracts a card from the inventory on play

--- pages/api/play/[username].js
+++ pages/api/play/[username].js
@@ -1,35 +1,48 @@
 import { db } from '../../../server/model.js';
 import { generateDiceGame } from '../../../lib/dice.js';
+import { generateBingoGame } from '../../../lib/bingo.js';
+import { getLadyLuck } from '../../../lib/ladyLuck.js';
+
 
 export default async function handler(req, res) {
-
   if (req.method === 'GET') {
+    let username = req.query.username;
+    let card_name = req.query.card_name;
+
     const query = { text: '', values: [] };
 
     query.text = `
-    SELECT COALESCE(quantity, 0)
-    FROM
-    (SELECT quantity
+    SELECT COALESCE(quantity, 0) as quantity, id_user
     FROM card_inventory
     WHERE id_user = (SELECT id FROM users WHERE username =  $1)
-    AND card_name = $2) quantity
+    AND card_name = $2
     `;
-    query.values = [req.query.username, req.query.card_name];
+    query.values = [username, card_name];
 
     const queryResponse = await db.query(query);
-    const cards = queryResponse.rows[0].coalesce;
+    const queryData = queryResponse.rows[0];
+    // console.log(queryResponse);
+    // const cards = queryResponse.rows[0].coalesce;
 
     function createResponse () {
       let responseBody = {
         game: {},
-        cards: cards - 1
+        cards: queryData.quantity
       };
-      if (cards > 0) {
-        switch (req.query.card_name) {
+
+      if (queryData && queryData.quantity > 0) {
+        switch (card_name) {
           case 'highroller':
             responseBody.game = generateDiceGame();
-            return responseBody;
+            break;
+          case 'bingo':
+            responseBody.game = generateBingoGame();
+            break;
+          case 'ladyluck':
+            responseBody.game = getLadyLuck();
+            break;
         }
+        useCard(queryData.id_user, card_name);
       }
       return responseBody;
     }
@@ -40,3 +53,16 @@ export default async function handler(req, res) {
   }
 }
 
+function useCard (id_user, card_name) {
+  const query = { text: '', values: [] };
+
+  query.text = `
+    UPDATE card_inventory
+    SET quantity = quantity - 1
+    WHERE id_user = $1
+    AND card_name = $2
+  `;
+  query.values = [id_user, card_name];
+
+  db.query(query);
+}
