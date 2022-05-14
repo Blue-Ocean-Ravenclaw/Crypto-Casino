b8d4ad4b23f02e4ad59c07b11b8dfaccce3a9976
Michael Washburn <m.washburn@mail.com>
2022-05-14 10:46:14 -0700

    fixed linter errors

--- pages/store/index.jsx
+++ pages/store/index.jsx
@@ -1,36 +1,33 @@
-import { useState, useEffect, useContext } from "react";
-import { useAppContext } from "../../context/state.js";
-import axios from "axios";
+/* eslint-disable no-shadow */
+/* eslint-disable import/extensions */
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import IconButton from "@mui/material/IconButton";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import GlobalStyles from "@mui/material/GlobalStyles";
-import Container from "@mui/material/Container";
-import Modal from "@mui/material/Modal";
-import { games } from "../../context/games.js";
-import { useRouter } from "next/router";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import GlobalStyles from '@mui/material/GlobalStyles';
+import Container from '@mui/material/Container';
+import Modal from '@mui/material/Modal';
+import { useRouter } from 'next/router';
+import { games } from '../../context/games.js';
+import { useAppContext } from '../../context/state.js';
 
 const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   width: 360,
-  bgcolor: "background.paper",
+  bgcolor: 'background.paper',
   borderRadius: 2,
   boxShadow: 24,
   p: 1,
@@ -39,36 +36,33 @@ const style = {
     height: 40,
   },
   iconSpacing: {
-    display: "flex",
-    justifyContent: "space-evenly",
+    display: 'flex',
+    justifyContent: 'space-evenly',
     fontSize: 30,
   },
   media: {
     height: 0,
-    paddingTop: "56.25%", // 16:9,
-    marginTop: "30",
+    paddingTop: '56.25%', // 16:9,
+    marginTop: '30',
   },
 };
 
 function GameStore() {
   const [open, setOpen] = useState(false);
   const [gameCount, setGameCount] = useState(1);
-  const [gameTitle, setGameTitle] = useState("");
-  const [gameColor, setGameColor] = useState("");
+  const [gameTitle, setGameTitle] = useState('');
+  const [gameColor, setGameColor] = useState('');
   const [total, setTotal] = useState(0);
   const [game, setGame] = useState({});
   const router = useRouter();
 
-  const context = useAppContext();
   const { tokens } = useAppContext();
 
   const { stateResults, stateRenderWallet } = useAppContext();
 
   const handleOpen = (e) => {
     setOpen(true);
-    let gameObj = games.filter((game) => {
-      return game.title === e.target.name;
-    });
+    const gameObj = games.filter((game) => game.title === e.target.name);
     setGame(gameObj[0]);
     setGameTitle(e.target.name);
     setGameColor(gameObj[0].buttonColor);
@@ -78,11 +72,12 @@ function GameStore() {
   const handleClose = () => {
     setOpen(false);
     setGameCount(1);
-    setGameTitle("");
+    setGameTitle('');
     setTotal(0);
   };
 
   const handleDecrament = () => {
+    // eslint-disable-next-line no-unused-expressions
     gameCount <= 1 ? setGameCount(1) : setGameCount((prev) => prev - 1);
     setTotal(game.price * gameCount);
   };
@@ -94,17 +89,17 @@ function GameStore() {
 
   const handlePurchase = () => {
     if (total > tokens) {
-      console.log("YOU BROKE");
+      console.log('YOU BROKE');
     } else {
       axios
         .post(`/api/tokens/${stateResults.username}`, { tokens: total * -1 })
-        .then((res) => {
+        .then(() => {
           axios
             .put(`/api/cards/${stateResults.username}`, {
               card_name: game.dbTitle,
               quantity: gameCount,
             })
-            .then((res) => stateRenderWallet((prev) => !prev))
+            .then(() => stateRenderWallet((prev) => !prev))
             .catch((err) => console.log(err));
         })
         .catch((err) => console.log(err));
@@ -124,7 +119,7 @@ function GameStore() {
   return (
     <>
       <GlobalStyles
-        styles={{ ul: { margin: 0, padding: 0, listStyle: "none" } }}
+        styles={{ ul: { margin: 0, padding: 0, listStyle: 'none' } }}
       />
       <Container maxWidth="md" sx={{ mt: 2, mb: 15 }}>
         <Grid container spacing={2} alignItems="flex-end">
@@ -136,8 +131,8 @@ function GameStore() {
               sm={6}
               md={4}
               sx={{
-                display: "flex",
-                justifyContent: "center",
+                display: 'flex',
+                justifyContent: 'center',
               }}
             >
               <Card
@@ -148,7 +143,7 @@ function GameStore() {
                 <CardContent
                   sx={{
                     padding: 0,
-                    position: "relative",
+                    position: 'relative',
                   }}
                 >
                   <CardMedia
@@ -170,7 +165,7 @@ function GameStore() {
                     sx={{
                       fontWeight: 600,
                       bgcolor: game.buttonColor,
-                      "&:hover": {
+                      '&:hover': {
                         bgcolor: game.buttonColor,
                       },
                     }}
@@ -192,7 +187,7 @@ function GameStore() {
                 <IconButton onClick={handleDecrament}>
                   <RemoveIcon style={style.largeIcon} />
                 </IconButton>
-                <span style={{ fontSize: "50px" }}>{gameCount}</span>
+                <span style={{ fontSize: '50px' }}>{gameCount}</span>
                 <IconButton onClick={handleIncrement}>
                   <AddIcon style={style.largeIcon} />
                 </IconButton>
@@ -204,7 +199,11 @@ function GameStore() {
                 component="h2"
                 style={style.iconSpacing}
               >
-                # of {gameTitle} cards
+                # of
+                {' '}
+                {gameTitle}
+                {' '}
+                cards
               </Typography>
 
               <Typography
@@ -213,13 +212,15 @@ function GameStore() {
                 component="h2"
                 style={style.iconSpacing}
               >
-                {total} Tokens
+                {total}
+                {' '}
+                Tokens
               </Typography>
 
               <Button
                 fullWidth
                 variant="contained"
-                onClick={() => onLink("/play")}
+                onClick={() => onLink('/play')}
                 sx={{
                   bgcolor: gameColor,
                 }}
